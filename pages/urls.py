a779da9b18e9fdabd479ee90f84d2f9609a4cbe7
DeVonte G <dagray92@hotmail.com>
2022-11-12 20:41:39 -0500

    rtx templates

--- pages/urls.py
+++ pages/urls.py
@@ -4,6 +4,9 @@ from .views import (
     AboutPageView,
     Dev1,
     Dev2,
+    RTX3060,
+    RTX3070,
+    RTX3080,
 )
 
 urlpatterns=[
@@ -11,4 +14,7 @@ urlpatterns=[
     path('about/', AboutPageView.as_view(), name='about'),
     path('dev1/', Dev1.as_view(), name='dev1'),
     path('dev2/', Dev2.as_view(), name='dev2'),
+    path('rtx3060/', RTX3060.as_view(), name='rtx3060'),
+    path('rtx3070/', RTX3070.as_view(), name='rtx3070'),
+    path('rtx3080/', RTX3080.as_view(), name='rtx3080'),
 ]
